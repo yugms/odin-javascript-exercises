8c90684ee7fe7c9e0a4d5670db132f740ab3a2d2
Yugm Shah <yugms7@gmail.com>
2025-08-12 13:20:10 -0400

    exercise 6

--- 06_repeatString/repeatString.js
+++ 06_repeatString/repeatString.js
@@ -1,5 +1,10 @@
-const repeatString = function() {
-
+const repeatString = function (string, repeatTimes) {
+  if (repeatTimes < 0) return "ERROR";
+  output = ""
+  for (let i = 0; i < repeatTimes; i++) {
+    output += string;
+  };
+  return output;
 };
 
 // Do not edit below this line
